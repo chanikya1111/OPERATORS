17322f111d5943e4062c2d293b273cf9367dc33c
chanikya1111 <chanikyareddy1508@gmail.com>
2023-09-06 18:26:47 +0530

    operators added

---
+++ operators.py
@@ -0,0 +1,67 @@
+#operators
+
+#arthamatic operators(+,-,*,**,/,//,%)
+
+# '+' operator:
+'''
+a=2
+b=3
+c=9
+d=a+b+c
+print("sum value:",d)
+# '-'
+a=7
+b=2
+c=a-b
+print(c)
+# '*'
+a=4
+b=3
+c=a*b
+print(c)
+# '**'
+a=12
+b=3
+c=a**b
+print(c)
+# '/'
+a=12
+b=2
+c=a/b
+print(c)
+
+# '//'
+ab=3
+bc=10
+cd=a//b
+print(cd)
+x=2
+y=4
+z=y%x
+print(z)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
