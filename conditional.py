35dd2f50af2dfff919acd9dad0c9530fdff9c00a
chanikya1111 <chanikyareddy1508@gmail.com>
2023-09-06 18:50:53 +0530

    conditional and logical operatiors are added

---
+++ conditional.py
@@ -0,0 +1,69 @@
+# Conditional (And,Or,Not)
+'''
+a=23
+b=35
+c=40
+d=a-b
+e=b-c
+
+print(d>e and a<b)
+print(d>e or a<b)
+print(not a<b)
+'''
+#logical operations(==,!=,<,>,<=,>=)
+'''
+a=23
+b=32
+c=a==b
+print(c)
+
+d=76
+e=56
+f=(d!=e)
+print(f)
+a=34
+b=35
+c=a>b
+print(c)
+d=a<b
+print(d)
+a=34
+b=76
+c=a>=b
+d=a<=b
+print(c)
+print(d)
+'''
+
+#membership operators(in,not in)
+'''
+a="my ambition is to become data scienctist"
+b=("to" in a)
+print(b)
+c=("beside" not in a)
+print(c)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
